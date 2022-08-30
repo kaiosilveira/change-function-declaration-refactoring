b8e21c59e73314d278d05dad2030d23303981e57
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-30 19:11:57 +0100

    adding-a-parameter: update caller

--- adding-a-parameter/caller1.js
+++ adding-a-parameter/caller1.js
@@ -1,6 +1,8 @@
 const { Book } = require('./book');
 
-const customer = { id: 1, name: 'Kaio Silveira' };
+const customer1 = { id: 1, name: 'Kaio Silveira' };
+const customer2 = { id: 2, name: 'Enzo Silveira' };
 const book = new Book();
 
-book.addReservation(customer);
+book.zzNEW_addReservation(customer1, false);
+book.zzNEW_addReservation(customer2, true);
