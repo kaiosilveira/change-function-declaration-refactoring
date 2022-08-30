3581efa1c0800d596e50b42ca5817d92b953293b
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-30 20:41:03 +0100

    changing-a-parameter: introduce new inNewEngland function

--- changing-a-parameter/in-new-england.js
+++ changing-a-parameter/in-new-england.js
@@ -2,4 +2,8 @@ function inNewEngland(aCustomer) {
   return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
 }
 
-module.exports = { inNewEngland };
+function zzNEW_inNewEngland(stateCode) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
+}
+
+module.exports = { inNewEngland, zzNEW_inNewEngland };
--- changing-a-parameter/in-new-england.test.js
+++ changing-a-parameter/in-new-england.test.js
@@ -1,4 +1,4 @@
-const { inNewEngland } = require('./in-new-england');
+const { inNewEngland, zzNEW_inNewEngland } = require('./in-new-england');
 
 describe('inNewEngland', () => {
   it('should return false if a customer is not a new englander', () => {
@@ -9,5 +9,17 @@ describe('inNewEngland', () => {
   it('should return true if a customer is not a new englander', () => {
     const aCustomer = { id: 1, name: 'Kaio Silveira', address: { state: 'MA' } };
     expect(inNewEngland(aCustomer)).toEqual(true);
+  });
+});
+
+describe('zzNEW_inNewEngland', () => {
+  it('should return false if a customer is not a new englander', () => {
+    const stateCode = 'SP';
+    expect(zzNEW_inNewEngland(stateCode)).toEqual(false);
+  });
+
+  it('should return true if a customer is not a new englander', () => {
+    const stateCode = 'MA';
+    expect(zzNEW_inNewEngland(stateCode)).toEqual(true);
   });
 });
